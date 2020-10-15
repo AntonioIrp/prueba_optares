32888e5077ca0196f8c107d4893856305a4ec6c1
Antonio <aroldan@actisa.net>
2020-10-15 14:41:01 +0200

    no message

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/say-hello")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name = "name", required = true) String name){
+        return new Greeting("Hello ".concat(name));
+    }
+}
